ed682a5f474a012e0633cd6f5aa89b4718dedc73
Rocky7842 <eric.rocky7842@gmail.com>
2023-11-20 22:06:30 +0800

    KernelSU: Integrate manually
    * Running with kprobe is not functioning properly.
    
    Change-Id: I19aa1def265c40d1c28a6e642d15f1a71f354ed3

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2317,6 +2317,10 @@ static u32 ptrace_parent_sid(struct task_struct *task)
 	return sid;
 }
 
+#ifdef CONFIG_KSU
+extern bool is_ksu_transition(const struct task_security_struct *old_tsec,
+			      const struct task_security_struct *new_tsec);
+#endif
 static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			    const struct task_security_struct *old_tsec,
 			    const struct task_security_struct *new_tsec)
@@ -2330,6 +2334,11 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 
 	if (new_tsec->sid == old_tsec->sid)
 		return 0; /* No change in credentials */
+
+#ifdef CONFIG_KSU
+	if (is_ksu_transition(old_tsec, new_tsec))
+		return 0;
+#endif
 
 	/*
 	 * The only transitions we permit under NNP or nosuid
